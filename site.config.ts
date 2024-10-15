fe8806ac78d38208d3131404dd5e9c6454078201
yongx <ivan.xyong@gmail.com>
2024-10-15 23:30:54 +0800

    add learning page

--- site.config.ts
+++ site.config.ts
@@ -42,11 +42,10 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/learning': '120d189352fe8082a9ade74c5eeaae04',
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
